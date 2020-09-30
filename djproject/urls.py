482295fcae2388f854aa10f2a41c2d06dc365e2a
Anshul Sharma <55247501+Anshul758@users.noreply.github.com>
2020-10-01 00:59:32 +0530

    Update urls.py

--- djproject/urls.py
+++ djproject/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from djproject import views
+
+# https://docs.djangoproject.com/en/3.1/topics/db/models/
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/secondIndex', views.secondIndex)
 ]
